4962e1f2e3893b7f4c04b5ac74a834bb5e05d90e
naimahassan <hassannaima77@gmail.com>
2018-03-21 11:47:27 +0300

    created a profile model

--- cakey/models.py
+++ cakey/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length = 90)
+    image = models.ImageField(upload_to = 'profile', null = True, blank = True)
+    email = models.EmailField(null = True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+
+    def __str__(self):
+        return str(self.image)
+
+    def create_profile(self):
+        self.save
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.remove            
