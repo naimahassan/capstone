c3d33d201e5358393ca760ae0bdec170da90e989
naimahassan <hassannaima77@gmail.com>
2018-03-21 12:07:19 +0300

    added class method to class post

--- cakey/models.py
+++ cakey/models.py
@@ -55,3 +55,18 @@ class Post(models.Model):
     image = models.ImageField(upload_to = 'post/', null = True, blank = True)
     cake = models.ForeignKey(Cake, on_delete=models.CASCADE, null = True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE , null = True)
+
+    def __str__(self):
+        return str(self.image)
+
+    def create_post(self):
+        self.save
+
+    def delete_post(self):
+        self.delete        
+    
+
+    @classmethod
+    def get_post(cls):
+        post=Post.objects.all()
+        return post    
