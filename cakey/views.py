0eedc96810f518a0025dd80b56837a9654f68e66
naimahassan <hassannaima77@gmail.com>
2018-03-21 12:14:50 +0300

    adding in the index page

--- cakey/views.py
+++ cakey/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Post,Profile,Cake
 import datetime as dt 
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    cake = Cake.get_cake()
+    post = Post.get_post()
+    return render(request, 'index.html',{"cake":cake,"post":post})
+
+    
