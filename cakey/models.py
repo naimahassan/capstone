4cf1f005ee4d1e1d63025d06084bc141aea1209f
naimahassan <hassannaima77@gmail.com>
2018-03-21 12:50:10 +0300

    delete a profile in post

--- cakey/models.py
+++ cakey/models.py
@@ -54,7 +54,7 @@ class Post(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE, null = True)
     image = models.ImageField(upload_to = 'post/', null = True, blank = True)
     cake = models.ForeignKey(Cake, on_delete=models.CASCADE, null = True)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE , null = True)
+    
 
     def __str__(self):
         return str(self.image)
