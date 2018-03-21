8b775938092c77ae739ac17cde74ee69f3b6f4c3
naimahassan <hassannaima77@gmail.com>
2018-03-21 12:25:25 +0300

    adding models to adminstration

--- cakey/admin.py
+++ cakey/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Profile,Post,Cake
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Cake)
+admin.site.register(Profile)
+
