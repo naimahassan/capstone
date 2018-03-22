6737260a4e9f925978ef62d5e89f7fd40ef0b5c9
naimahassan <hassannaima77@gmail.com>
2018-03-22 14:55:10 +0300

    added a detail function

--- cakey/views.py
+++ cakey/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Post,Profile,Recipe
+from . models import Post,Profile 
 import datetime as dt 
 from django.contrib.auth.models import User
-from . forms import NewRecipeForm
+from . forms import NewPostForm
 
 # Create your views here.
 def index(request):
-    recipe = Recipe.get_recipe()
     post = Post.objects.all()
-    return render(request, 'index.html',{"recipe":recipe,"post":post})
+    return render(request, 'index.html',{"post":post})
 
 def post(request):
     post = Post.get_post()
     return render(request, 'all-cakey/post.html',{"post":post})
 
-def recipe(request):
-    recipe = Recipe.objects.all()
+def detail(request,post_id):
+    post = Post.objects.get(id = post_id)
+    print(post)
+    return render(request, 'all-cakey/detail.html',{"post":post})           
+
+def profile(request,user_id):
+    current_user = request.user
+    profile = Profile.objects.all()
+    post = Post.objects.filter(profile__id=user_id)
+
+    return render(request, 'all-cakey/profile.html', {"current_user":current_user,"profile":profile,"post":post}) 
+
+
+def new_post(request):
     if request.method == 'POST':
-        form = NewRecipeForm(request.POST)
+        form = NewPostForm(request.POST, request.FILES)
         if form.is_valid():
-            recipe = form.save(commit=False)
-            recipe.save()
-            return redirect ('recipe')
+            post = form.save(commit=False)
+            post.save()
+            return redirect ('post')
     else:
-        form = NewRecipeForm()
-    return render(request, 'all-cakey/recipe.html',{"recipe":recipe})            
+        form = NewPostForm()
+    return render(request, 'all-cakey/new-post.html', {"form":form})       
+
+       
+
+
