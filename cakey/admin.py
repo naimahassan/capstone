4fdf5915b41439f1dab27eced5d02cf20c5d40f1
naimahassan <hassannaima77@gmail.com>
2018-03-22 14:52:48 +0300

    deleted a url

--- cakey/admin.py
+++ cakey/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from . models import Profile,Post,Recipe
+from . models import Profile,Post
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Recipe)
 admin.site.register(Profile)
 
